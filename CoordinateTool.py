7320eca1904df5cb28cc5a8d9ed423dbfa34c37a
Geotrixwb <114463481+Geotrixwb@users.noreply.github.com>
2022-09-26 22:28:31 -0700

    Add files via upload

---
+++ CoordinateTool.py
@@ -0,0 +1,30 @@
+import random
+
+
+print("all rights to the owners")
+
+randomSeedGen = random.random()
+print ("seed of the day: " +str(randomSeedGen), "-"+str(randomSeedGen))
+print("\n")
+print("please enter the correct X and Y coordinates")
+
+X_coordinates = float(input("Enter the X coordinates: "))
+Y_coordinates = float(input("Enter the Y coordinates: "))
+Z_coordinates = float(input("Enter the Z coordinates: "))
+
+def noDecimalInYcoord():
+ pass
+
+
+
+
+X_coord_div = float(X_coordinates / 8)
+Y_coord_div = int(Y_coordinates / 8)
+Z_coord_div = float(Z_coordinates / 8)
+
+
+
+print("coordinates to the nether or overworld:")
+for coordinates in X_coord_div, Y_coord_div, Z_coord_div:
+    print("X: " +str(X_coord_div), "  " "Y: "+str(Y_coord_div),"   " "Z: " +str(Z_coord_div))
+
